10034df7f45b6e9a271c65bc371c44e9c857f709
tg1523342 <tg1523342@icloud.com>
2025-09-05 18:00:23 -0400

    Refactored drive subsystem code

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -18,11 +18,13 @@ import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds;
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.Constants.DriveConstants;
+import pabeles.concurrency.ConcurrencyOps.Reset;
 
 /*
  * This is still a WIP
@@ -30,66 +32,83 @@ import frc.robot.Constants.DriveConstants;
 
 public class DriveSubsystem extends SubsystemBase {
 
-  //this is where I make the lists of devices like motors and encoders, and configs for the motors
-  private ArrayList<SparkMax> sparkMaxs = new ArrayList<>();
-  private ArrayList<RelativeEncoder> encoders = new ArrayList<>();
-  private ArrayList<SparkMaxConfig> configs = new ArrayList<>();
-  
-  //sets an array for motor id's
-  private int[] idArray = { 
-    DriveConstants.kFrontLeftMotorId, 
-    DriveConstants.kFrontRightMotorId, 
-    DriveConstants.kBackRightMotorId, 
-    DriveConstants.kBackLeftMotorId
-  };
+  public enum DriveMotor {
+    kFrontLeft, kFrontRight, kBackRight, kBackLeft;
+
+    public int getId() {
+      return switch (this) {
+        case kFrontLeft -> DriveConstants.kFrontLeftMotorId;
+        case kFrontRight -> DriveConstants.kFrontRightMotorId;
+        case kBackRight -> DriveConstants.kBackRightMotorId;
+        case kBackLeft -> DriveConstants.kBackLeftMotorId;
+      };
+    }
+
+    public int getIndex() {
+      return switch (this) {
+        case kFrontLeft -> 0;
+        case kFrontRight -> 1;
+        case kBackRight -> 2;
+        case kBackLeft -> 3;
+      };
+    }
+
+    public boolean getInverted() {
+      return switch (this) {
+        case kFrontLeft -> DriveConstants.kFrontLeftMotorInverted;
+        case kFrontRight -> DriveConstants.kFrontRightMotorInverted;
+        case kBackRight -> DriveConstants.kBackRightMotorInverted;
+        case kBackLeft -> DriveConstants.kBackLeftMotorInverted;
+      };
+    }
+  }
+
+  // this is where I make the lists of devices like motors and encoders, and
+  // configs for the motors
+  private SparkMax[] motors = new SparkMax[4];
+  private RelativeEncoder[] encoders = new RelativeEncoder[4];
 
   private ShuffleboardTab driveTab = Shuffleboard.getTab(getName());
 
   public DriveSubsystem() {
+    DriveMotor[] motorPoses = DriveMotor.values();
 
     // this creates the objects and stores them in array
-    for (int i = 0; i < idArray.length; i++) {
-      configs.add(new SparkMaxConfig());
-      sparkMaxs.add(new SparkMax(idArray[i], MotorType.kBrushless));
-      sparkMaxs.get(i).configure(configs.get(i), ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-      encoders.add(sparkMaxs.get(i).getEncoder());
+    for (int i = 0; i < motorPoses.length; i++) {
+      motors[i] = new SparkMax(motorPoses[i].getId(), MotorType.kBrushless);
+      encoders[i] = motors[i].getEncoder();
     }
 
     for (RelativeEncoder relativeEncoder : encoders) {
       relativeEncoder.setPosition(0); // this will change, and need to figureout how to get velocity
-
     }
 
-    applyAllConfigs(config -> {
+    configureAllMotors(config -> {
       // sets the idle mode and the smart current limit
       config.smartCurrentLimit(DriveConstants.kSmartLimit);
       config.idleMode(DriveConstants.kMotorIdleMode);
 
-      //sets the PID 
+      // sets the PID
       config.closedLoop.pid(DriveConstants.kVelocityP, DriveConstants.kVelocityI, DriveConstants.kVelocityD);
       config.closedLoop.velocityFF(DriveConstants.kVelocityFF);
-    });
+    }, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
 
-    //sets if an motor is inverted
-    setConfig(DriveConstants.kFrontRightMotorId, config -> config.inverted(DriveConstants.kFrontRightMotorInverted));
-    setConfig(DriveConstants.kFrontLeftMotorId, config -> config.inverted(DriveConstants.kFrontLeftMotorInverted));
-    setConfig(DriveConstants.kBackRightMotorId, config -> config.inverted(DriveConstants.kBackRightMotorInverted));
-    setConfig(DriveConstants.kBackLeftMotorId, config -> config.inverted(DriveConstants.kBackLeftMotorInverted));
-
-    
-
+    // sets if an motor is inverted
+    for (DriveMotor motorPose : DriveMotor.values()) {
+      configureMotor(motorPose, config -> config.inverted(motorPose.getInverted()), ResetMode.kNoResetSafeParameters,
+          PersistMode.kPersistParameters);
+    }
   }
-  
+
   /**
    * 
    * @param speeds
    */
   public void drive(ChassisSpeeds speeds) {
     speeds = new ChassisSpeeds(
-      -speeds.vxMetersPerSecond, 
-      speeds.vyMetersPerSecond, 
-      -speeds.omegaRadiansPerSecond
-    );
+        -speeds.vxMetersPerSecond,
+        speeds.vyMetersPerSecond,
+        -speeds.omegaRadiansPerSecond);
 
     DifferentialDriveWheelSpeeds wheelSpeeds;
 
@@ -114,23 +133,26 @@ public class DriveSubsystem extends SubsystemBase {
 
   }
 
-  //stops all the motors
+  // stops all the motors
   public void stopDrive() {
     applyAllMotors(motor -> motor.set(0));
   }
-  
-  //sets all idlemodes to be brake
+
+  // sets all idlemodes to be brake
   public void setBrakeMode() {
-    applyAllConfigs(config -> config.idleMode(IdleMode.kBrake));
+    configureAllMotors(config -> config.idleMode(IdleMode.kBrake), ResetMode.kNoResetSafeParameters,
+        PersistMode.kPersistParameters);
   }
 
-  //sets all idlemodes to be coast
+  // sets all idlemodes to be coast
   public void setCoastMode() {
-    applyAllConfigs(config -> config.idleMode(IdleMode.kCoast));
+    configureAllMotors(config -> config.idleMode(IdleMode.kCoast), ResetMode.kNoResetSafeParameters,
+        PersistMode.kPersistParameters);
   }
 
   /**
    * This will take the speed param and set all the motor's to that speed
+   * 
    * @param speed
    */
   public void setAllSpeed(double speed) {
@@ -138,82 +160,71 @@ public class DriveSubsystem extends SubsystemBase {
   }
 
   /**
-   * @param motorId
-   * @param function
-   */
-  public void setConfig(int motorId, Consumer<SparkMaxConfig> function) {
-    for (int i = 0; i < sparkMaxs.size(); i++) {
-      if (sparkMaxs.get(i).getDeviceId() == motorId) {
-        function.accept(configs.get(i));
-      }
-    }
-  }
-
-  /**
    * 
-   * @param motorId
+   * @param motorPose
    * @param function
    */
-  public void setMotor(int motorId, Consumer<SparkMax> function) {
-    for (int i = 0; i < sparkMaxs.size(); i++) {
-      if (sparkMaxs.get(i).getDeviceId() == motorId) {
-        function.accept(sparkMaxs.get(i));
-      }
-    }
+  private SparkMax getMotor(DriveMotor motorPose) {
+    return motors[motorPose.getIndex()];
   }
 
   /**
    * 
    * @param function
    */
-  public void applyAllMotors(Consumer<SparkMax> function) {
-    for (int i = 0; i < sparkMaxs.size(); i++) {
-      function.accept(sparkMaxs.get(i));
+  private void applyAllMotors(Consumer<SparkMax> function) {
+    for (SparkMax motor : motors) {
+      function.accept(motor);
     }
   }
 
   /**
    * 
-   * @param function
+   * @param configure
    */
-  public void applyAllConfigs(Consumer<SparkMaxConfig> function) { 
-    for (int i = 0; i < configs.size(); i++) {
-      function.accept(configs.get(i));
-    }
+  private void configureAllMotors(Consumer<SparkMaxConfig> configure, ResetMode resetMode, PersistMode persist) {
+    applyAllMotors(motor -> {
+      SparkMaxConfig config = new SparkMaxConfig();
+      configure.accept(config);
+
+      motor.configure(config, resetMode, persist);
+    });
   }
 
   /**
    * 
-   * @param id
+   * @param configure
+   */
+  private void configureMotor(DriveMotor motorPose, Consumer<SparkMaxConfig> configure, ResetMode resetMode,
+      PersistMode persist) {
+    SparkMax motor = getMotor(motorPose);
+    SparkMaxConfig config = new SparkMaxConfig();
+    configure.accept(config);
+
+    motor.configure(config, resetMode, persist);
+  }
+
+  /**
+   * 
+   * @param motorPose
    * @return
    */
-  public double getSpeed(int id) {
-    for (SparkMax motor : sparkMaxs) {
-      if (motor.getDeviceId() == id) {
-        return motor.get();
-      }
-    }
-
-    return 0;
+  public double getSpeed(DriveMotor motorPose) {
+    return encoders[motorPose.getIndex()].getVelocity();
   }
 
   /**
    * 
-   * @param motorId
+   * @param motorPose
    * @return
    */
-  public RelativeEncoder getEncoder(int motorId) {
-    for (int i = 0; i < sparkMaxs.size(); i++) {
-      if (sparkMaxs.get(i).getDeviceId() == motorId) {
-        return encoders.get(i);
-      }
-    }
-    return null;
+  private RelativeEncoder getEncoder(int motorPose) {
+    return encoders[motorPose];
   }
 
   @Override
   public void periodic() {
-    //updates 
+    // updates
   }
 
 }
