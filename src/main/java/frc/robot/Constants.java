9af4d33a85e2aae501034b50e4f807039c418b25
tg1523342 <tg1523342@icloud.com>
2025-09-13 17:35:11 -0400

    Added constants for encoders and auton

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -6,37 +6,54 @@ package frc.robot;
 
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 
+import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
+import edu.wpi.first.math.util.Units;
+
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants
+ * should be declared
  * globally (i.e. public static). Do not put anything functional in this class.
  *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
-  public static class OperatorConstants {
+  private Constants() {
+    throw new UnsupportedOperationException("This is a utility class!");
+  }
 
-    //Ports for the controllers, and json file relitive path
+  public static class OperatorConstants {
+    private OperatorConstants() {
+      throw new UnsupportedOperationException("This is a utility class!");
+    }
+
+    // Ports for the controllers, and json file relative path
     public static final int kDriverControllerPort = 0;
-    public static final int kCoDriverControllerPort = 0;
+    public static final int kCoDriverControllerPort = 1;
     public static final String kDriverJsonPath = "src/main/java/frc/robot/assets/data/mainDriverBinds/mainDriver.json";
     public static final String kCoDriverJsonPath = "src/main/java/frc/robot/assets/data/coDriverBinds/coDriver.json";
   }
 
   public static class DriveConstants {
+    private DriveConstants() {
+      throw new UnsupportedOperationException("This is a utility class!");
+    }
 
-    // id's for the motors
-    public static final int kFrontLeftMotorId = 0;
-    public static final int kFrontRightMotorId = 1;
+    // ids for the motors
+    public static final int kFrontLeftMotorId = 3;
+    public static final int kFrontRightMotorId = 5;
     public static final int kBackLeftMotorId = 2;
-    public static final int kBackRightMotorId = 3;
+    public static final int kBackRightMotorId = 4;
 
     // sets if an motor is inverted
     public static final boolean kFrontLeftMotorInverted = false;
-    public static final boolean kFrontRightMotorInverted = false;
+    public static final boolean kFrontRightMotorInverted = true;
     public static final boolean kBackLeftMotorInverted = false;
-    public static final boolean kBackRightMotorInverted = false;
+    public static final boolean kBackRightMotorInverted = true;
 
     // idle mode
     public static final IdleMode kMotorIdleMode = IdleMode.kBrake;
@@ -44,26 +61,39 @@ public final class Constants {
     // smartLimit
     public static final int kSmartLimit = 30;
 
-    // public static final double kMaxSpeedMetersPerSecond = 
-    //   ((MotorConstants.kNeoMotorMaxRPM / 60.0) / kGearRatio) * kWheelCircumference;
-    public static final double kMaxSpeedMetersPerSecond = 5.6;
+    // public static final double kMaxSpeedMetersPerSecond =
+    // ((MotorConstants.kNeoMotorMaxRPM / 60.0) / kGearRatio) * kWheelCircumference;
+    public static final double kMaxPhysicalSpeedMetersPerSecond = 5.6;
 
     // PID constants for controlling wheel velocity
     public static final double kVelocityP = 0.1;
     public static final double kVelocityI = 0.0;
     public static final double kVelocityD = 0.0;
-    public static final double kVelocityFF = 1 / kMaxSpeedMetersPerSecond;
+    public static final double kVelocityFF = 1 / kMaxPhysicalSpeedMetersPerSecond;
 
-    // this makes it so if there any stick drift it prevents the robot from move due to the stick drift
-    public static final double kDeadBand = 0;    
+    // physical constants
+    public static final double kWheelDiameter = Units.inchesToMeters(6);
+    public static final double kWheelCircumference = kWheelDiameter * Math.PI;
+    public static final double kGearRatio = 8.45865;
 
-    public static final double kDriverSensitvity = 0;
-    public static final double kRotatonSenitvity = 0;
+    public static final double kTrackWidth = 0.5;
 
-    public static final double kMaxForwardSpeed = 0;
-    public static final double kMaxRotationSpeed = 0;
+    // encoder conversion factors
+    public static final double kRotationsToMeters = kWheelCircumference / kGearRatio;
+    public static final double KRotationsPerMinuteToMetersPerSecond = kRotationsToMeters / 60;
 
-    public static final double kDriveKinematics = 0;
-    
+    // kinematics
+    public static final DifferentialDriveKinematics kDriveKinematics = new DifferentialDriveKinematics(kTrackWidth);
+
+    // this makes it so if there any stick drift it prevents the robot from move due
+    // to the stick drift
+    public static final double kDeadBand = 0.01;
+
+    public static final double kDriverSensitvity = 0.4;
+    public static final double kRotatonSenitvity = 0.4;
+
+    public static final double kMaxForwardSpeed = 1;
+    public static final double kMaxRotationSpeed = 0.7;
+
   }
 }
