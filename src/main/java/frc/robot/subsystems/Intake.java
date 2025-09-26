98b7336d563f726700cf957df21a12a660f48879
whiteswordv <143036168+whiteswordv@users.noreply.github.com>
2025-09-25 23:04:09 -0400

    Update Intake.java

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -7,16 +7,22 @@ import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.config.SparkMaxConfig;
 
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants.CubeIntakeConstants;
+import frc.robot.Constants.IntakeConstants;
 
+/*
+ *  A subsystem that controls the intake motor's
+ */
 public class Intake extends SubsystemBase {
     
     private SparkMax m_leaderMotor, m_followerMotor;
     private RelativeEncoder m_Encoder;
 
+    /*
+     * Constructs intake
+     */
     public Intake() {
-
         SparkMaxConfig config = new SparkMaxConfig();
 
         m_leaderMotor = new SparkMax(IntakeConstants.kLeaderMotorId, MotorType.kBrushless);
@@ -32,16 +38,37 @@ public class Intake extends SubsystemBase {
         m_leaderMotor.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
 
         config
-            .inverted(CubeIntakeConstants.kFollowerMotorInverted)
+            .inverted(IntakeConstants.kFollowerMotorInverted)
             .follow(m_leaderMotor.getDeviceId());
         
         m_followerMotor.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
     }
 
-    @Override
-    public void periodic() {
-
+    /**
+     * Sets the speed of the two motors.
+     * @param speed <- passes the speed into the motor.
+     */
+    public void setSpeed(double speed) {
+        m_leaderMotor.set(speed);
+    }
+    
+    /**
+     * Returns a command to intake.
+     * @return
+     */
+    public Command intake() {
+        return runEnd(
+            () -> setSpeed(0), 
+            () -> setSpeed(0));
     }
 
-
+    /**
+     * Returns a command to outtake.
+     * @return
+     */
+    public Command deploy() {
+        return runEnd(
+            () -> setSpeed(0),
+            () -> setSpeed(0));
+    }
 }
