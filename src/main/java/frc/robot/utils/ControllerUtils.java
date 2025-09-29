f7e53e4fbe04917372648396163b2608a1e73e91
tg1523342 <tg1523342@icloud.com>
2025-09-29 19:37:56 -0400

    Updated axisSensitivity documentation

--- src/main/java/frc/robot/utils/ControllerUtils.java
+++ src/main/java/frc/robot/utils/ControllerUtils.java
@@ -13,16 +13,14 @@ public class ControllerUtils {
     }
 
     /**
-     * Transforms a raw axis input based on a sensitivity, deadband, and
-     * multiplier.
+     * Applies a sensitivity curve to a controller axis value.
      * 
      * @param raw         The raw value from the axis in the range [-1, 1].
      * @param sensitivity The sensitivity factor. 1 does nothing, less than 1 makes
      *                    it less sensitive, greater than 1 makes it more sensitive.
      * @param max         The raw value will be multiplied by this after applying
-     *                    deadband and sensitivity such that it will be the max
-     *                    output value
-     * @return The transformed axis value in the range [-max, max].
+     *                    sensitivity such that it will be the max output value
+     * @return The adjusted axis value in the range [-max, max].
      */
     public static double axisSensitivity(double raw, double sensitivity, double max) {
         return max * Math.signum(raw) * Math.pow(raw, 1 / sensitivity);
