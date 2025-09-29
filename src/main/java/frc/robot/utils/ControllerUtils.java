3efd50f666fd428b34c96ef85a575cf47a224c8b
tg1523342 <tg1523342@icloud.com>
2025-09-29 19:34:53 -0400

    Removed deadband from axisTransform

--- src/main/java/frc/robot/utils/ControllerUtils.java
+++ src/main/java/frc/robot/utils/ControllerUtils.java
@@ -19,14 +19,11 @@ public class ControllerUtils {
      * @param raw         The raw value from the axis in the range [-1, 1].
      * @param sensitivity The sensitivity factor. 1 does nothing, less than 1 makes
      *                    it less sensitive, greater than 1 makes it more sensitive.
-     * @param deadband    The deadband threshold in the range [0, 1]. When the raw
-     *                    value is less than this, the output will be 0.
      * @param multiplier  The raw value will be multiplied by this after applying
      *                    deadband and sensitivity.
      * @return The transformed axis value in the range [-1, 1].
      */
-    public static double axisTransform(double raw, double sensitivity, double deadband, double multiplier) {
-        return multiplier * Math.signum(raw)
-                * Math.pow(Math.max(0, (Math.abs(raw) - deadband) / (1 - deadband)), 1 / sensitivity);
+    public static double axisTransform(double raw, double sensitivity, double multiplier) {
+        return multiplier * Math.signum(raw) * Math.pow(raw, 1 / sensitivity);
     }
 }
