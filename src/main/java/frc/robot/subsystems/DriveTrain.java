7f58b54e6bdb7496f444bd27e057ee81a28e932d
tg1523342 <tg1523342@icloud.com>
2025-10-01 16:18:21 -0400

    Added PID, odometry, and pathplanner support

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -10,12 +10,22 @@ import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.TalonSRXConfiguration;
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.studica.frc.AHRS;
+import com.studica.frc.AHRS.NavXComType;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.MecanumDriveOdometry;
+import edu.wpi.first.math.kinematics.MecanumDriveWheelPositions;
+import edu.wpi.first.math.kinematics.MecanumDriveWheelSpeeds;
 import edu.wpi.first.wpilibj.drive.MecanumDrive;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.AutoConstants;
 import frc.robot.Constants.DriveConstants;
 import frc.robot.utils.ControllerUtils;
 
@@ -37,8 +47,11 @@ public class DriveTrain extends SubsystemBase {
     // calculates the wheel speeds based on the inputs
     private final MecanumDrive m_drive;
 
+    // gyro sensor
+    private final AHRS m_gyro = new AHRS(NavXComType.kMXP_SPI);
+
     // calculates odometry
-    // private final MecanumDriveOdometry m_odometry;
+    private final MecanumDriveOdometry m_odometry;
 
     // displays robot position on field
     private final Field2d m_field = new Field2d();
@@ -51,7 +64,11 @@ public class DriveTrain extends SubsystemBase {
     public DriveTrain() {
         TalonSRXConfiguration config = new TalonSRXConfiguration();
 
-        config.continuousCurrentLimit = DriveConstants.kSmartCurrentLimit;
+        config.slot0.kP = DriveConstants.kVelocityP;
+        config.slot0.kI = DriveConstants.kVelocityI;
+        config.slot0.kD = DriveConstants.kVelocityD;
+
+        config.continuousCurrentLimit = DriveConstants.kContinuousCurrentLimit;
 
         // left side motord
         m_frontLeftMotor.configAllSettings(config);
@@ -89,25 +106,35 @@ public class DriveTrain extends SubsystemBase {
         m_backRightMotorFollower.follow(m_backRightMotor);
 
         m_drive = new MecanumDrive(
-                speed -> m_frontLeftMotor.set(ControlMode.PercentOutput, speed),
-                speed -> m_backLeftMotor.set(ControlMode.PercentOutput, speed),
-                speed -> m_frontRightMotor.set(ControlMode.PercentOutput, speed),
-                speed -> m_backRightMotor.set(ControlMode.PercentOutput, speed));
+                speed -> m_frontLeftMotor.set(ControlMode.Velocity,
+                        speed / DriveConstants.kRawPer100msToMetersPerSecond),
+                speed -> m_backLeftMotor.set(ControlMode.Velocity,
+                        speed / DriveConstants.kRawPer100msToMetersPerSecond),
+                speed -> m_frontRightMotor.set(ControlMode.Velocity,
+                        speed / DriveConstants.kRawPer100msToMetersPerSecond),
+                speed -> m_backRightMotor.set(ControlMode.Velocity,
+                        speed / DriveConstants.kRawPer100msToMetersPerSecond));
 
-        // m_odometry = new MecanumDriveOdometry(DriveConstants.kDriveKinematics, m_gyro.getRotation2d(),
-        //         getWheelPositions());
+        m_odometry = new MecanumDriveOdometry(DriveConstants.kDriveKinematics,
+                m_gyro.getRotation2d(),
+                getWheelPositions());
 
         // we handle the deadband ourselves
         m_drive.setDeadband(0);
 
-        // m_driveTab.addNumber("Robot X (m)", () -> m_odometry.getPoseMeters().getX());
-        // m_driveTab.addNumber("Robot Y (m)", () -> m_odometry.getPoseMeters().getY());
-        // m_driveTab.addNumber("Robot Heading (deg)", () -> m_odometry.getPoseMeters().getRotation().getDegrees());
+        // we want drive to pass speed as meters per second
+        m_drive.setMaxOutput(DriveConstants.kMaxSpeedMetersPerSecond);
+
+        m_driveTab.addNumber("Robot X (m)", () -> m_odometry.getPoseMeters().getX());
+        m_driveTab.addNumber("Robot Y (m)", () -> m_odometry.getPoseMeters().getY());
+        m_driveTab.addNumber("Robot Heading (deg)", () -> m_odometry.getPoseMeters().getRotation().getDegrees());
 
         m_driveTab.add("Field", m_field);
 
-        // AutoBuilder.configure(m_odometry::getPoseMeters, this::resetOdometry, this::getChassisSpeeds,
-        //         this::driveRobotRelative, AutoConstants.kAutoController, AutoConstants.kRobotConfig, () -> false, this);
+        AutoBuilder.configure(m_odometry::getPoseMeters, this::resetOdometry,
+                this::getChassisSpeeds,
+                this::driveRobotRelative, AutoConstants.kAutoController,
+                AutoConstants.kRobotConfig, () -> false, this);
     }
 
     /**
@@ -121,7 +148,7 @@ public class DriveTrain extends SubsystemBase {
      *                  Counterclockwise is positive.
      */
     public void drive(double xSpeed, double ySpeed, double zRotation) {
-        m_drive.driveCartesian(xSpeed, ySpeed, zRotation);
+        m_drive.driveCartesian(xSpeed, ySpeed, zRotation, m_gyro.getRotation2d());
     }
 
     /**
@@ -129,14 +156,18 @@ public class DriveTrain extends SubsystemBase {
      * 
      * @param speeds
      */
-    // public void driveRobotRelative(ChassisSpeeds speeds) {
-    //     MecanumDriveWheelSpeeds wheelSpeeds = DriveConstants.kDriveKinematics.toWheelSpeeds(speeds);
+    public void driveRobotRelative(ChassisSpeeds speeds) {
+        MecanumDriveWheelSpeeds wheelSpeeds = DriveConstants.kDriveKinematics.toWheelSpeeds(speeds);
 
-    //     m_frontLeftClosedLoop.setReference(wheelSpeeds.frontLeftMetersPerSecond, ControlType.kVelocity);
-    //     m_backLeftClosedLoop.setReference(wheelSpeeds.rearLeftMetersPerSecond, ControlType.kVelocity);
-    //     m_frontRightClosedLoop.setReference(wheelSpeeds.frontRightMetersPerSecond, ControlType.kVelocity);
-    //     m_backRightClosedLoop.setReference(wheelSpeeds.rearRightMetersPerSecond, ControlType.kVelocity);
-    // }
+        m_frontLeftMotor.set(ControlMode.Velocity, wheelSpeeds.frontLeftMetersPerSecond
+                / DriveConstants.kRawPer100msToMetersPerSecond);
+        m_backLeftMotor.set(ControlMode.Velocity, wheelSpeeds.rearLeftMetersPerSecond
+                / DriveConstants.kRawPer100msToMetersPerSecond);
+        m_frontRightMotor.set(ControlMode.Velocity, wheelSpeeds.frontRightMetersPerSecond
+                / DriveConstants.kRawPer100msToMetersPerSecond);
+        m_backRightMotor.set(ControlMode.Velocity, wheelSpeeds.rearRightMetersPerSecond
+                / DriveConstants.kRawPer100msToMetersPerSecond);
+    }
 
     /**
      * Creates a {@link Command} that drives the robot based on joystick or axis
@@ -188,17 +219,17 @@ public class DriveTrain extends SubsystemBase {
      * 
      * @return The current robot relative chassis speeds.
      */
-    // public ChassisSpeeds getChassisSpeeds() {
-    //     MecanumDriveWheelSpeeds wheelSpeeds = new MecanumDriveWheelSpeeds(
-    //             m_frontLeftEncoder.getVelocity(),
-    //             m_frontRightEncoder.getVelocity(),
-    //             m_backLeftEncoder.getVelocity(),
-    //             m_backRightEncoder.getVelocity());
+    public ChassisSpeeds getChassisSpeeds() {
+        MecanumDriveWheelSpeeds wheelSpeeds = new MecanumDriveWheelSpeeds(
+                m_frontLeftMotor.getSelectedSensorVelocity() * DriveConstants.kRawPer100msToMetersPerSecond,
+                m_frontRightMotor.getSelectedSensorVelocity() * DriveConstants.kRawPer100msToMetersPerSecond,
+                m_backLeftMotor.getSelectedSensorVelocity() * DriveConstants.kRawPer100msToMetersPerSecond,
+                m_backRightMotor.getSelectedSensorVelocity() * DriveConstants.kRawPer100msToMetersPerSecond);
 
-    //     ChassisSpeeds speeds = DriveConstants.kDriveKinematics.toChassisSpeeds(wheelSpeeds);
+        ChassisSpeeds speeds = DriveConstants.kDriveKinematics.toChassisSpeeds(wheelSpeeds);
 
-    //     return speeds;
-    // }
+        return speeds;
+    }
 
     /**
      * Gets the current wheel positions in meters.
@@ -206,22 +237,22 @@ public class DriveTrain extends SubsystemBase {
      * @return The {@link MecanumDriveWheelPositions} representing the current wheel
      *         positions in meters.
      */
-    // public MecanumDriveWheelPositions getWheelPositions() {
-    //     return new MecanumDriveWheelPositions(
-    //             m_frontLeftEncoder.getPosition(),
-    //             m_frontRightEncoder.getPosition(),
-    //             m_backLeftEncoder.getPosition(),
-    //             m_backRightEncoder.getPosition());
-    // }
+    public MecanumDriveWheelPositions getWheelPositions() {
+        return new MecanumDriveWheelPositions(
+                m_frontLeftMotor.getSelectedSensorPosition() * DriveConstants.kRawToMeters,
+                m_frontRightMotor.getSelectedSensorPosition() * DriveConstants.kRawToMeters,
+                m_backLeftMotor.getSelectedSensorPosition() * DriveConstants.kRawToMeters,
+                m_backRightMotor.getSelectedSensorPosition() * DriveConstants.kRawToMeters);
+    }
 
     /**
      * Resets the current tracked robot position to the specified pose.
      * 
      * @param pose The {@link Pose2d} to reset the position to.
      */
-    // public void resetOdometry(Pose2d pose) {
-    //     m_odometry.resetPosition(m_gyro.getRotation2d(), getWheelPositions(), pose);
-    // }
+    public void resetOdometry(Pose2d pose) {
+        m_odometry.resetPosition(m_gyro.getRotation2d(), getWheelPositions(), pose);
+    }
 
     /**
      * Sets the {@link NeutralMode} for all drivetrain motors. This will not persist
@@ -238,9 +269,9 @@ public class DriveTrain extends SubsystemBase {
 
     @Override
     public void periodic() {
-        // m_odometry.update(m_gyro.getRotation2d(), getWheelPositions());
+        m_odometry.update(m_gyro.getRotation2d(), getWheelPositions());
 
-        // m_field.setRobotPose(m_odometry.getPoseMeters());
+        m_field.setRobotPose(m_odometry.getPoseMeters());
     }
 
     @Override
