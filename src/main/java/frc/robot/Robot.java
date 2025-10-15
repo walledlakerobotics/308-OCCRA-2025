6100e24c4b020f764079dccc75ae3d9f0a67528d
tg1523342 <tg1523342@icloud.com>
2025-10-15 19:19:53 -0400

    Added pathfinding warmup command

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,6 +4,7 @@
 
 package frc.robot;
 
+import com.pathplanner.lib.commands.PathfindingCommand;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 
 import edu.wpi.first.wpilibj.TimedRobot;
@@ -29,6 +30,8 @@ public class Robot extends TimedRobot {
         // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
         // autonomous chooser on the dashboard.
         m_robotContainer = new RobotContainer();
+
+        PathfindingCommand.warmupCommand().schedule();
     }
 
     /**
