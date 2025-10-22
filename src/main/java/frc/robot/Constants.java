92a4b17b25b69028a6dc014b7f8dc4a65408630b
tg1523342 <tg1523342@icloud.com>
2025-10-22 12:49:47 -0400

    Set CAN IDs and feedforward constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -46,10 +46,10 @@ public final class Constants {
         }
 
         // ids for the motors
-        public static final int kFrontLeftMotorId = 3;
-        public static final int kRearLeftMotorId = 2;
-        public static final int kFrontRightMotorId = 5;
-        public static final int kRearRightMotorId = 4;
+        public static final int kFrontLeftMotorId = 4;
+        public static final int kRearLeftMotorId = 3;
+        public static final int kFrontRightMotorId = 2;
+        public static final int kRearRightMotorId = 5;
 
         // sets if an motor is inverted
         public static final boolean kLeftMotorsInverted = false;
@@ -70,9 +70,9 @@ public final class Constants {
         public static final double kVelocityP = 0.1;
         public static final double kVelocityI = 0.0;
         public static final double kVelocityD = 0.0;
-        public static final double kVelocityS = 1;
-        public static final double kVelocityV = 1;
-        public static final double kVelocityA = 1;
+        public static final double kVelocityS = 0;
+        public static final double kVelocityV = 0;
+        public static final double kVelocityA = 0;
 
         // PID constants for controlling robot rotation
         public static final double kRotationP = 0.3;
